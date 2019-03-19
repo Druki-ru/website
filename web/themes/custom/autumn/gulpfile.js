f2e49884662e4687fc26008542a37767590acff8
Niklan <niklanrus@gmail.com>
2019-03-19 18:23:37 +0500

    Start theming.

---
+++ web/themes/custom/autumn/gulpfile.js
@@ -0,0 +1,30 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let autoprefixer = require('autoprefixer');
+let postcss = require('gulp-postcss');
+let lost = require('lost');
+
+gulp.task('sass', function() {
+  return gulp.src([
+    'assets/scss/generic/generic.scss',
+    'assets/scss/elements/elements.scss',
+    'assets/scss/objects/objects.scss',
+    'assets/scss/components/components.scss',
+    'assets/scss/utilities/utilities.scss',
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      lost(),
+      autoprefixer()
+    ]))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('assets/scss/**', gulp.parallel('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'sass:watch'));
