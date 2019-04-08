4c387e33f81a99a3a1e4a692eeec38bb6d3659ad
Niklan <niklanrus@gmail.com>
2019-04-08 18:18:27 +0500

    Created Condition plugin for Druki content. Added Block Visibility Group module for handling lazy builded blocks.

---
+++ web/modules/custom/druki_content/src/Plugin/Condition/DrukiContent.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\druki_content\Plugin\Condition;
+
+use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\druki_content\Entity\DrukiContentInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Druki Content' condition.
+ *
+ * @Condition(
+ *   id = "druki_content",
+ *   label = @Translation("Druki Content Page"),
+ * )
+ */
+class DrukiContent extends ConditionPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * Creates a new DrukiContent instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $current_route_match
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentRouteMatch $current_route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routeMatch = $current_route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): object {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary(): string {
+    return $this->t('Druki content page');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate(): bool {
+    $parameter = $this->routeMatch->getParameter('druki_content');
+
+    if ($parameter instanceof DrukiContentInterface) {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+}
