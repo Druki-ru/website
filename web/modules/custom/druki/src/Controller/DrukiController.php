e7643ee03946e6bfc593eda6c761b2c2c6921ed5
Niklan <niklanrus@gmail.com>
2019-04-10 19:49:39 +0500

    Download page WIP

---
+++ web/modules/custom/druki/src/Controller/DrukiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\druki\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Returns responses for Druki routes.
+ */
+class DrukiController extends ControllerBase {
+
+  /**
+   * Builds the response.
+   */
+  public function buildDownload() {
+
+    $build['content'] = [
+      '#theme' => 'druki_download',
+    ];
+
+    return $build;
+  }
+
+}
