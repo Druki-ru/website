9e29e171b1f4907f9a1f458132ae88ab26e24732
Niklan <niklanrus@gmail.com>
2019-04-18 19:57:15 +0500

    Fix webhook

--- web/modules/custom/druki_git/src/Controller/DrukiGitController.php
+++ web/modules/custom/druki_git/src/Controller/DrukiGitController.php
@@ -59,7 +59,7 @@ class DrukiGitController extends ControllerBase implements ContainerInjectionInt
   public function webhook(): JsonResponse {
     $webhook_info = json_decode($this->request->getContent());
 
-    if ($this->git->init() && $webhook_info['object_kind'] == 'push') {
+    if ($this->git->init() && $webhook_info->object_kind == 'push') {
       if ($this->git->pull()) {
         $this->logger->info(t('Webhook is triggered, and content is successfully pulled.'));
       }
