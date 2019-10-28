8a0813bea575498e7083fcc90af779b3b748d64a
Niklan <niklanrus@gmail.com>
2019-10-28 19:28:22 +0500

    New theme WIP

---
+++ web/themes/custom/loremaster/gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const postcssCustomMedia = require('postcss-custom-media');
+const postcssInlineSvg = require('postcss-inline-svg');
+
+gulp.task('sass', function() {
+  return gulp.src([
+    'assets/scss/generic/generic.scss',
+    'assets/scss/elements/elements.scss',
+    'assets/scss/objects/objects.scss',
+    'assets/scss/components/components.scss',
+    'assets/scss/utilities/utilities.scss',
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({includePaths: ['../../../../node_modules']}).on('error', sass.logError))
+    .pipe(postcss([
+      postcssInlineSvg({
+        paths: ['assets/images/icons']
+      }),
+      postcssCustomMedia(),
+      autoprefixer(),
+    ]))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('assets/scss/**', gulp.parallel('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'sass:watch'));
