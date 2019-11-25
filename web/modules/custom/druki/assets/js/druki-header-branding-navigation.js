425141f1cd25af093b5c2c138dd89cc3104a228e
Niklan <niklanrus@gmail.com>
2019-11-25 19:23:40 +0500

    WIP

---
+++ web/modules/custom/druki/assets/js/druki-header-branding-navigation.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Druki Header branding navigation behaviors.
+ */
+(function (Drupal) {
+
+  Drupal.behaviors.drukiHeaderBrandingNavigation = {
+    attach: function (context, settings) {
+       this.attachNavigation(context, settings);
+    },
+
+    attachNavigation: function (context, settings) {
+      const burgerEl = context.querySelector('.js-header-branding-burger');
+      const dropdownEl = context.querySelector('.js-header-branding-dropdown');
+
+      if (burgerEl && !burgerEl.processed && dropdownEl) {
+        burgerEl.processed = true;
+
+        const clickListener = () => {
+          dropdownEl.classList.toggle('druki-header-branding-navigation__dropdown--active')
+        };
+        burgerEl.addEventListener('click', clickListener);
+      }
+    }
+  };
+
+})(Drupal);
