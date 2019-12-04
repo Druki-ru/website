b5225aedfccdf951930738e8d056b3d69b21c61a
Niklan <niklanrus@gmail.com>
2019-12-04 18:48:14 +0500

    Follow up clean up in query helper.

--- web/modules/custom/druki_search/src/SearchPage/QueryHelper.php
+++ web/modules/custom/druki_search/src/SearchPage/QueryHelper.php
@@ -6,11 +6,20 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\search_api\ParseMode\ParseModePluginManager;
 use Drupal\search_api\Utility\QueryHelperInterface;
 
+/**
+ * Provides query helper for global search.
+ */
 class QueryHelper {
 
-  public const FULL = 'druki_search.search_page.full';
+  /**
+   * Marks query as full search, without being filtered.
+   */
+  const FULL = 'druki_search.search_page.full';
 
-  public const FILTERED = 'druki_search.search_page.filtered';
+  /**
+   * The query tipe which can be filtered not directly by others.
+   */
+  const FILTERED = 'druki_search.search_page.filtered';
 
   /**
    * The query helper.
@@ -26,12 +35,43 @@ class QueryHelper {
    */
   protected $parseModeManager;
 
+  /**
+   * The index storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $indexStorage;
+
+  /**
+   * Constructs a new QueryHelper object.
+   *
+   * @param \Drupal\search_api\Utility\QueryHelperInterface $query_helper
+   *   The search api query helper.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\search_api\ParseMode\ParseModePluginManager $parse_mode_plugin_manager
+   *   The parse mode plugin manager.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
   public function __construct(QueryHelperInterface $query_helper, EntityTypeManagerInterface $entity_type_manager, ParseModePluginManager $parse_mode_plugin_manager) {
     $this->queryHelper = $query_helper;
     $this->indexStorage = $entity_type_manager->getStorage('search_api_index');
     $this->parseModeManager = $parse_mode_plugin_manager;
   }
 
+  /**
+   * Gets instance of query.
+   *
+   * @param string $search_id
+   *   The search id.
+   *
+   * @return \Drupal\search_api\Query\QueryInterface
+   *   The query.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
   public function getQuery($search_id = self::FULL) {
     $index = $this->indexStorage->load('global');
     $query = $this->queryHelper->createQuery($index);
