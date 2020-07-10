31d76f030a3c2f2f269dc958aa3831d4c88ea309
Niklan <niklanrus@gmail.com>
2020-07-10 17:25:58 +0500

    Issue #101. Markdown module uninstalled and all realted code and comments are deleted.

--- web/modules/custom/druki_markdown/src/Parser/MarkdownParser.php
+++ web/modules/custom/druki_markdown/src/Parser/MarkdownParser.php
@@ -8,8 +8,6 @@ use League\CommonMark\Environment;
 
 /**
  * Provides default Markdown parser.
- *
- * @todo consider is it need to be a service since it no more uses DI.
  */
 class MarkdownParser implements MarkdownParserInterface {
 
@@ -24,12 +22,6 @@ class MarkdownParser implements MarkdownParserInterface {
    * Constructs a new MarkdownParser object.
    */
   public function __construct() {
-    // The CommonMark used directly instead of 'markdown' service, because
-    // the Markdown module and it's service require to have special text
-    // filter and user permissions to use extensions. In our case this is
-    // overhead and not needed, since we use parsed HTML in content and filter
-    // become usless. But all other staff defined according to Markdown module
-    // for correctly working with filter.
     $environment = Environment::createCommonMarkEnvironment();
     $environment->addExtension(new DrukiParserExtensions());
     $this->markdownParser = new CommonMarkConverter([], $environment);
