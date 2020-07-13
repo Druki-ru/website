e4169455d921d97dc8585a4509310f1caccd15e9
Niklan <niklanrus@gmail.com>
2020-07-13 17:34:53 +0500

    Issue #107. Fixed errors in druki_media module.

--- web/modules/custom/druki_media/src/Plugin/Field/FieldFormatter/RemoteVideoOptimizedFormatter.php
+++ web/modules/custom/druki_media/src/Plugin/Field/FieldFormatter/RemoteVideoOptimizedFormatter.php
@@ -2,14 +2,11 @@
 
 namespace Drupal\druki_media\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\media\OEmbed\ResourceFetcherInterface;
-use Drupal\media\OEmbed\UrlResolverInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -23,7 +20,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   }
  * )
  */
-class RemoteVideoOptimizedFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+final class RemoteVideoOptimizedFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
 
   /**
    * The URL resolver.
@@ -47,74 +44,15 @@ class RemoteVideoOptimizedFormatter extends FormatterBase implements ContainerFa
   private $responsiveStyleStorage;
 
   /**
-   * OptimizedRemoteVideoFormatter constructor.
-   *
-   * @param string $plugin_id
-   *   The plugin_id for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings.
-   * @param \Drupal\media\OEmbed\UrlResolverInterface $url_resolver
-   *   The URL resolver.
-   * @param \Drupal\media\OEmbed\ResourceFetcherInterface $resource_fetcher
-   *   The resource fetcher.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $responsive_image_style_storage
-   *   The responsive image style storage.
-   */
-  public function __construct(
-    $plugin_id,
-    $plugin_definition,
-    FieldDefinitionInterface $field_definition,
-    array $settings,
-    $label,
-    $view_mode,
-    array $third_party_settings,
-    UrlResolverInterface $url_resolver,
-    ResourceFetcherInterface $resource_fetcher,
-    EntityStorageInterface $responsive_image_style_storage
-  ) {
-
-    parent::__construct(
-      $plugin_id,
-      $plugin_definition,
-      $field_definition,
-      $settings,
-      $label,
-      $view_mode,
-      $third_party_settings
-    );
-
-    $this->responsiveStyleStorage = $responsive_image_style_storage;
-
-    $this->urlResolver = $url_resolver;
-    $this->resourceFetcher = $resource_fetcher;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): object {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('media.oembed.url_resolver'),
-      $container->get('media.oembed.resource_fetcher'),
-      $container->get('entity_type.manager')->getStorage('responsive_image_style')
-    );
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->urlResolver = $container->get('media.oembed.url_resolver');
+    $instance->resourceFetcher = $container->get('media.oembed.resource_fetcher');
+    $instance->responsiveStyleStorage = $container->get('entity_type.manager')->getStorage('responsive_image_style');
+
+    return $instance;
   }
 
   /**
@@ -122,8 +60,8 @@ class RemoteVideoOptimizedFormatter extends FormatterBase implements ContainerFa
    */
   public static function defaultSettings(): array {
     return [
-        'thumbnail_style' => '',
-      ] + parent::defaultSettings();
+      'thumbnail_style' => '',
+    ];
   }
 
   /**
