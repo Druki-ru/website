4eb373d55f170c01019efd9d059e920a79ae0e7b
Niklan <niklanrus@gmail.com>
2020-10-08 16:58:59 +0500

    Resolve #17. Added link to project repository to the header.

---
+++ web/modules/custom/druki/src/Plugin/Block/HeaderRepositoryButton.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\druki\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+
+/**
+ * Provides a project repository button block.
+ *
+ * @Block(
+ *   id = "druki_header_repository_button",
+ *   admin_label = @Translation("Header repository button"),
+ *   category = @Translation("Druki")
+ * )
+ */
+final class HeaderRepositoryButton extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#type' => 'link',
+      '#url' => Url::fromUri('https://github.com/Druki-ru/website'),
+      '#title' => NULL,
+      '#attributes' => [
+        'class' => [
+          'header-repository-button',
+        ],
+        'target' => '_blank',
+        'rel' => 'noopener nofollow',
+        'title' => new TranslatableMarkup('Project repository'),
+        'aria-label' => new TranslatableMarkup('Project repository'),
+      ],
+    ];
+    return $build;
+  }
+
+}
