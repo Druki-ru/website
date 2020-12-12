24a080e96dcfae516e2b77c6acf9e2f386047443
Niklan <niklanrus@gmail.com>
2020-12-12 11:42:06 +0500

    Resolve #34: Imagemagick argumens moved from hook to event.

---
+++ web/modules/custom/druki/src/EventSubscriber/ImagemagickEventSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\druki\EventSubscriber;
+
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Drupal\imagemagick\Event\ImagemagickExecutionEvent;
+
+/**
+ * Provides Imagemagick event subscriber.
+ */
+final class ImagemagickEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      ImagemagickExecutionEvent::PRE_CONVERT_EXECUTE => 'preConvertExecute',
+    ];
+  }
+
+  /**
+   * Fires before the 'convert' command is executed.
+   *
+   * @param \Drupal\imagemagick\Event\ImagemagickExecutionEvent $event
+   *   The execution event.
+   */
+  public function preConvertExecute(ImagemagickExecutionEvent $event): void {
+    $arguments = $event->getExecArguments();
+    // https://developers.google.com/speed/docs/insights/OptimizeImages
+    $arguments->add('-sampling-factor 4:2:0');
+    // Progression JPEG and interlaced PNGs support.
+    $arguments->add('-interlace Plane');
+    // Clean image for all unused data. I.e. EXIF.
+    $arguments->add('-strip');
+  }
+
+}
