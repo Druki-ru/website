b43a8a3ee163d271de48e4ec4628880ea8eb5b93
Niklan <niklanrus@gmail.com>
2020-12-15 20:25:31 +0500

    Added HeaderRepositoryButtonTest.

---
+++ tests/src/ExistingSite/Plugin/Block/HeaderRepositoryButtonTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Druki\Tests\ExistingSite\Plugin\Block;
+
+use Drupal\Core\Url;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Provides test for header repository button.
+ * 
+ * @coversDefaultClass \Drupal\druki\Plugin\Block\HeaderRepositoryButton
+ */
+final class HeaderRepositoryButtonTest extends ExistingSiteBase {
+
+  /**
+   * Tests that button is rendered on page.
+   */
+  public function testButtonIsOnPage(): void {
+    $this->drupalGet(Url::fromRoute('<front>'));
+    $this->assertSession()->elementExists('css', '.header-repository-button');
+  }
+
+}
