7495d6365c1c8b365c24d0a2f21b8fb2c2a68d7c
Niklan <niklanrus@gmail.com>
2020-12-15 20:35:39 +0500

    Added HeaderSearchBlockTest.

---
+++ tests/src/ExistingSite/Plugin/Block/HeaderSearchBlockTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Druki\Tests\ExistingSite\Plugin\Block;
+
+use Drupal\Core\Url;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Provides test for header search block.
+ *
+ * @coversDefaultClass \Drupal\druki\Plugin\Block\HeaderSearchBlock
+ */
+final class HeaderSearchBlockTest extends ExistingSiteBase {
+
+  /**
+   * Tests that search is rendered on page.
+   */
+  public function testSearchIsOnPage(): void {
+    $this->drupalGet(Url::fromRoute('<front>'));
+    $this->assertSession()->elementExists('css', 'druki-header-search');
+  }
+
+}
