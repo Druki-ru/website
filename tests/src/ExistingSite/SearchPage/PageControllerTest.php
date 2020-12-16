d238b65a7895aa65d15f4d856b133abee210e8e3
Niklan <niklanrus@gmail.com>
2020-12-16 21:47:40 +0500

    PageControllerTest is now using fake entities.

--- tests/src/ExistingSite/SearchPage/PageControllerTest.php
+++ tests/src/ExistingSite/SearchPage/PageControllerTest.php
@@ -2,18 +2,20 @@
 
 namespace Druki\Tests\ExistingSite\SearchPage;
 
+use Druki\Tests\Traits\DrukiContentCreationTrait;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
+use Drupal\search_api\Entity\Index;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
  * Provides test search page.
  *
  * @coversDefaultClass \Drupal\druki_search\SearchPage\PageController
- *
- * @todo Use fake entities when trait will be created.
  */
 final class PageControllerTest extends ExistingSiteBase {
+
+  use DrukiContentCreationTrait;
 
   /**
    * Tests empty request.
@@ -50,7 +52,29 @@ final class PageControllerTest extends ExistingSiteBase {
    * Tests request with results found.
    */
   public function testResults(): void {
-    $keys = 'Drupal';
+    $content = $this->createDrukiContent();
+
+    $index = Index::load('global');
+    $datasource_id = 'entity:' . $content->getEntityTypeId() . '/' . $content->id() . ':' . $content->language()->getId();
+    $index->indexSpecificItems([$datasource_id => $content->getTypedData()]);
+
+    $keys = $content->label();
+    $url = Url::fromRoute('druki_search.page', [], [
+      'query' => [
+        'text' => $content->label(),
+      ],
+    ]);
+    $this->drupalGet($url);
+    $this->assertSession()->statusCodeEquals(200);
+    $page_title = new TranslatableMarkup('Search results for "%keys"', ['%keys' => $keys]);
+    // Remove <em> wrapper for placeholder, this is markup, not text.
+    $page_title = \strip_tags($page_title);
+    $this->assertSession()->pageTextContainsOnce($page_title);
+    $this->assertSession()->elementExists('css', '.search-results__item');
+    $this->assertSession()->pageTextContains($content->toUrl()->setAbsolute()->toString());
+
+    // Test that this entity can be found using 'search_keywords'.
+    $keys = $content->get('search_keywords')->first()->value;
     $url = Url::fromRoute('druki_search.page', [], [
       'query' => [
         'text' => $keys,
@@ -63,6 +87,7 @@ final class PageControllerTest extends ExistingSiteBase {
     $page_title = \strip_tags($page_title);
     $this->assertSession()->pageTextContainsOnce($page_title);
     $this->assertSession()->elementExists('css', '.search-results__item');
+    $this->assertSession()->pageTextContains($content->toUrl()->setAbsolute()->toString());
   }
 
 }
