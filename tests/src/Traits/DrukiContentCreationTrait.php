5c6e3fadca8acd9857a5d15c96804819aba0eae9
Niklan <niklanrus@gmail.com>
2020-12-16 21:22:22 +0500

    Added DrukiContentCreationTrait and removed unusd phpunit params.

---
+++ tests/src/Traits/DrukiContentCreationTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Druki\Tests\Traits;
+
+use Drupal\druki_content\Entity\DrukiContent;
+use Drupal\druki_content\Entity\DrukiContentInterface;
+use Drupal\Tests\RandomGeneratorTrait;
+use weitzman\DrupalTestTraits\DrupalTrait;
+
+/**
+ * Provides methods to create druki content.
+ */
+trait DrukiContentCreationTrait {
+
+  use RandomGeneratorTrait;
+  use DrupalTrait;
+
+  /**
+   * Creates a druki content.
+   *
+   * @param array $values
+   *   An associative array with values for entity.
+   *
+   * @return \Drupal\druki_content\Entity\DrukiContentInterface
+   *   The created entity.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function createDrukiContent(array $values = []): DrukiContentInterface {
+    $values += [
+      'title' => $this->randomMachineName(32),
+      'external_id' => $this->randomMachineName(32),
+      'relative_pathname' => 'docs/' . $this->randomMachineName() . '.md',
+      'search_keywords' => [
+        $this->randomMachineName(),
+        $this->randomMachineName(),
+      ],
+    ];
+
+    $content = DrukiContent::create($values);
+    $content->save();
+
+    $this->markEntityForCleanup($content);
+
+    return $content;
+  }
+
+}
