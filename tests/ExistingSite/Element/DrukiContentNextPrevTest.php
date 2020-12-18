a352b089b9fea8398b6900b0e5f96ababe54f3a0
Niklan <niklanrus@gmail.com>
2020-12-18 19:27:17 +0500

    Added DrukiContentNextPrevTest.

---
+++ tests/ExistingSite/Element/DrukiContentNextPrevTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Druki\Tests\ExistingSite\Element;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Provides test for druki content next and previous links.
+ *
+ * @covers \Drupal\druki_content\Element\DrukiContentNextPrev
+ */
+final class DrukiContentNextPrevTest extends ExistingSiteBase {
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\Renderer
+   */
+  protected $renderer;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->renderer = $this->container->get('renderer');
+  }
+
+  /**
+   * Test behavior when no links provided.
+   */
+  public function testNoLinks(): void {
+    $element = [
+      '#type' => 'druki_content_next_prev',
+    ];
+    $html = $this->renderer->renderPlain($element);
+    $this->assertEmpty($html);
+  }
+
+  /**
+   * Test behavior when both links are presented.
+   */
+  public function testBothLink(): void {
+    $url = Url::fromRoute('<front>')->setAbsolute();
+    $prev = Link::fromTextAndUrl('Previous', $url);
+    $next = Link::fromTextAndUrl('Next', $url);
+
+    $element = [
+      '#type' => 'druki_content_next_prev',
+      '#prev_link' => $prev,
+      '#next_link' => $next,
+    ];
+
+    $html = $this->renderer->renderPlain($element);
+    $this->assertContains('Previous', $html);
+    $this->assertContains('Next', $html);
+    $this->assertContains($url->toString(), $html);
+  }
+
+}
