0857f8aa54289c49d949d204f51e6b2f5a643f4b
Niklan <niklanrus@gmail.com>
2021-01-29 17:27:24 +0500

    Cleanup theme after PostCSS migration.

--- web/themes/custom/loremaster/gulpfile.js
+++ web/themes/custom/loremaster/gulpfile.js
@@ -1,15 +1,20 @@
-const gulp = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssCustomMedia = require('postcss-custom-media');
-const postcssInlineSvg = require('postcss-inline-svg');
 const postcssImport = require('postcss-import');
 const postcssNesting = require('postcss-nesting');
+const postcssInlineSvg = require('postcss-inline-svg');
 
-gulp.task('build:css', function() {
+/**
+ * Builds CSS files from PostCSS.
+ *
+ * @return {*}
+ */
+function buildCss() {
   let plugins = [
     postcssImport(),
     postcssNesting(),
@@ -17,24 +22,28 @@ gulp.task('build:css', function() {
       paths: ['assets/images/icons']
     }),
     postcssCustomMedia({
-      importFrom: 'css/01-generic/media-breakpoints.pcss',
+      importFrom: 'assets/pcss/01-generic/media-breakpoints.pcss',
     }),
     autoprefixer(),
     cssnano({ preset: 'default' }),
   ];
 
-  return gulp.src([
-    'css/**/*.pcss',
-  ])
+  return src(['assets/pcss/**/*.pcss'])
     .pipe(sourcemaps.init())
     .pipe(postcss(plugins))
     .pipe(rename({
       extname: '.css',
     }))
     .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('dist/css/'));
-});
+    .pipe(dest('assets/css/'));
+}
 
-gulp.task('watch:css', function() {
-  gulp.watch('css/**/*.pcss', gulp.parallel('build:css'));
-});
+/**
+ * Watch for SCSS file changes and build CSS if any of them changed.
+ */
+function watchCss() {
+  watch('assets/pcss/**/*.pcss', parallel(buildCss));
+}
+
+exports.buildCss = buildCss;
+exports.default = exports.watchCss = watchCss;
