a0dd4e6419bf2603bcd6eb19d2c2a4fdadecccf7
Niklan <niklanrus@gmail.com>
2021-03-25 18:19:12 +0500

    Resolve #44: Improved dark theme switcher.

--- web/modules/custom/druki/src/Plugin/Block/HeaderDarkModeSwitcher.php
+++ web/modules/custom/druki/src/Plugin/Block/HeaderDarkModeSwitcher.php
@@ -20,59 +20,11 @@ final class HeaderDarkModeSwitcher extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration() {
-    return [
-      'class' => 'header-dark-mode-switcher',
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-
-    $form['class'] = [
-      '#type' => 'textfield',
-      '#title' => new TranslatableMarkup('CSS class'),
-      '#required' => TRUE,
-      '#default_value' => $this->configuration['class'],
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    parent::blockSubmit($form, $form_state);
-    $this->configuration['class'] = $form_state->getValue('class');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-    $class = $this->configuration['class'];
-
+  public function build(): array {
+    $build = [];
     $build['content'] = [
-      '#type' => 'html_tag',
-      '#tag' => 'button',
-      '#attributes' => [
-        'class' => [
-          'js-dark-mode-switcher',
-          $class,
-        ],
-        'aria-label' => new TranslatableMarkup('Switch between dark and light styles'),
-      ],
-      '#attached' => [
-        'library' => [
-          'druki/dark-mode',
-        ],
-      ],
+      '#theme' => 'druki_dark_mode_toggle',
     ];
-
     return $build;
   }
 
