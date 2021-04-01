6db87c86b21e70883e509eeffd07dabd1134e45c
Niklan <niklanrus@gmail.com>
2021-04-01 20:15:00 +0500

    WIP new layout.

--- web/modules/custom/druki/src/Form/FrontpageSettings.php
+++ web/modules/custom/druki/src/Form/FrontpageSettings.php
@@ -8,14 +8,14 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Configure Druki user guide settings.
+ * Provides settings form for frontpage.
  */
-final class OfficialUserGuideSettingsForm extends ConfigFormBase {
+final class FrontpageSettings extends ConfigFormBase {
 
   /**
-   * The state key for media entity storage.
+   * The state key for download media image.
    */
-  public const IMAGE_STORAGE_KEY = 'druki_official_user_guide_media';
+  public const DOWNLOAD_MEDIA_STORAGE_KEY = 'druki_frontpage_download_media';
 
   /**
    * The state storage.
@@ -41,7 +41,7 @@ final class OfficialUserGuideSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container): object {
+  public static function create(ContainerInterface $container) {
     $instance = parent::create($container);
     $instance->entityTypeManager = $container->get('entity_type.manager');
     $instance->responsiveImageStyleHelper = $container->get('druki.helper.responsive_image_style');
@@ -52,44 +52,37 @@ final class OfficialUserGuideSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId(): string {
-    return 'druki_official_user_guide_settings';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    $settings = $this->config('druki.official_user_guide_settings');
     $form['#tree'] = TRUE;
+    $settings = $this->config('druki.frontpage.settings');
 
-    $form['image'] = [
+    $form['download'] = [
       '#type' => 'fieldset',
-      '#title' => new TranslatableMarkup('Image'),
+      '#title' => new TranslatableMarkup('Download'),
     ];
 
-    $default_image = NULL;
-    if ($media_id = $this->state->get(self::IMAGE_STORAGE_KEY)) {
+    $download_media_default_media = NULL;
+    if ($download_image_media_id = $this->state->get(self::DOWNLOAD_MEDIA_STORAGE_KEY)) {
       $media_storage = $this->entityTypeManager->getStorage('media');
-      if ($media = $media_storage->load($media_id)) {
-        $default_image = $media;
+      if ($download_media = $media_storage->load($download_image_media_id)) {
+        $download_media_default_media = $download_media;
       }
     }
 
-    $form['image']['media'] = [
+    $form['download']['media'] = [
       '#type' => 'entity_autocomplete',
       '#target_type' => 'media',
       '#selection_settings' => [
         'target_bundles' => ['image'],
       ],
       '#title' => new TranslatableMarkup('Image'),
-      '#default_value' => $default_image,
+      '#default_value' => $download_media_default_media,
     ];
 
-    $form['image']['style'] = [
+    $form['download']['image_style'] = [
       '#type' => 'select',
       '#options' => $this->responsiveImageStyleHelper->getOptions(),
-      '#default_value' => $settings->get('image_style'),
+      '#default_value' => $settings->get('download.image_style'),
       '#title' => new TranslatableMarkup('Image style'),
     ];
 
@@ -100,18 +93,18 @@ final class OfficialUserGuideSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
-    $responsive_image_style_id = $form_state->getValue(['image', 'style']);
-    if ($responsive_image_style_id) {
-      $this->config('druki.official_user_guide_settings')
-        ->set('image_style', $responsive_image_style_id)
+    $download_media_image_style_id = $form_state->getValue(['download', 'image_style']);
+    if ($download_media_image_style_id) {
+      $this->config('druki.frontpage.settings')
+        ->set('download.image_style', $download_media_image_style_id)
         ->save();
     }
 
-    if ($media_id = $form_state->getValue(['image', 'media'])) {
-      $this->state->set(self::IMAGE_STORAGE_KEY, $media_id);
+    if ($download_media_id = $form_state->getValue(['download', 'media'])) {
+      $this->state->set(self::DOWNLOAD_MEDIA_STORAGE_KEY, $download_media_id);
     }
     else {
-      $this->state->delete(self::IMAGE_STORAGE_KEY);
+      $this->state->delete(self::DOWNLOAD_MEDIA_STORAGE_KEY);
     }
 
     parent::submitForm($form, $form_state);
@@ -120,8 +113,15 @@ final class OfficialUserGuideSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
+  public function getFormId(): string {
+    return 'druki_frontpage_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   protected function getEditableConfigNames(): array {
-    return ['druki.official_user_guide_settings'];
+    return ['druki.frontpage.settings'];
   }
 
 }
