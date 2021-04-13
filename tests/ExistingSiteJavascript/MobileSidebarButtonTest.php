bec666d7036160e61f42e81bb7e70fa496c6966c
Niklan <niklanrus@gmail.com>
2021-04-13 16:57:00 +0500

    Issue #47: Refactor MobileSidebarButtonTest for new theme.

--- tests/ExistingSiteJavascript/MobileSidebarButtonTest.php
+++ tests/ExistingSiteJavascript/MobileSidebarButtonTest.php
@@ -16,8 +16,6 @@ final class MobileSidebarButtonTest extends ExistingSiteSelenium2DriverTestBase 
    * Tests that button is exists and works.
    */
   public function testButton(): void {
-    $this->markTestSkipped('This test must be improved in https://github.com/Druki-ru/website/issues/47');
-
     // If we install website from configuration, there will be 0 menu items.
     // We need to create them. But this is not such an important test, so it
     // skipped to push PHPUnit into GitHub Actions.
@@ -26,24 +24,15 @@ final class MobileSidebarButtonTest extends ExistingSiteSelenium2DriverTestBase 
     $this->getDriverInstance()->resizeWindow(360, 640);
 
     $assert_session = $this->assertSession();
-    $sidebar_button = $assert_session->elementExists('css', '.js-mobile-sidebar-button');
-    $sidebar = $assert_session->elementExists('css', '.js-mobile-sidebar');
+    $sidebar_button = $assert_session->elementExists('css', '[data-mobile-hamburger]');
+    $sidebar = $assert_session->elementExists('css', '.page__mobile-dropdown');
 
     // Check that button toggle visibility of the sidebar.
     $this->assertEquals(FALSE, $sidebar->isVisible());
     $sidebar_button->click();
     $this->assertEquals(TRUE, $sidebar->isVisible());
 
-    // Click on overlay must close sidebar.
-    $overlay = $assert_session->elementExists('css', '.js-mobile-sidebar-overlay');
-    $overlay->click();
-    $this->assertEquals(FALSE, $sidebar->isVisible());
-
-    // Test that 'Close' button also works.
     $sidebar_button->click();
-    $this->assertEquals(TRUE, $sidebar->isVisible());
-    $close_button = $assert_session->elementExists('css', '.js-mobile-sidebar-close');
-    $close_button->click();
     $this->assertEquals(FALSE, $sidebar->isVisible());
   }
 
