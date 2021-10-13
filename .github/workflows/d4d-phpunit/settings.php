091a377b4edcb6fc37a3871dfc21958c379770de
Niklan <niklanrus@gmail.com>
2021-10-13 18:53:24 +0500

    Added PHP_FPM_CLEAR_ENV value.

--- .github/workflows/d4d-phpunit/settings.php
+++ .github/workflows/d4d-phpunit/settings.php
@@ -767,7 +767,7 @@ $settings['entity_update_batch_size'] = 50;
 #if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #  include $app_root . '/' . $site_path . '/settings.local.php';
 #}
-$databases['default']['default'] = array (
+$databases['default']['default'] = [
   'database' => getenv('DB_NAME'),
   'username' => getenv('DB_USER'),
   'password' => getenv('DB_PASSWORD'),
@@ -776,6 +776,6 @@ $databases['default']['default'] = array (
   'port' => getenv('DB_PORT'),
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => getenv('DB_DRIVER'),
-);
+];
 $settings['config_sync_directory'] = '../config/sync';
 $settings['skip_permissions_hardening'] = TRUE;
