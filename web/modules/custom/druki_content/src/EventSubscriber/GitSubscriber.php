c8babd67bcb675e3bedeaa9ee0d549b37d0b6782
Niklan <niklanrus@gmail.com>
2021-10-14 19:36:05 +0500

    Fixed wrong typehint.

--- web/modules/custom/druki_content/src/EventSubscriber/GitSubscriber.php
+++ web/modules/custom/druki_content/src/EventSubscriber/GitSubscriber.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\druki_content\EventSubscriber;
 
-use Drupal\druki_content\Data\ContentSyncQueueManager;
+use Drupal\druki_content\Queue\ContentSyncQueueManager;
 use Drupal\druki_git\Event\DrukiGitEvent;
 use Drupal\druki_git\Event\DrukiGitEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -20,7 +20,7 @@ final class GitSubscriber implements EventSubscriberInterface {
   /**
    * Constructs a new GitSubscriber object.
    *
-   * @param \Drupal\druki_content\Data\ContentSyncQueueManager $sync_queue_manager
+   * @param \Drupal\druki_content\Queue\ContentSyncQueueManager $sync_queue_manager
    *   The sync queue manager.
    */
   public function __construct(ContentSyncQueueManager $sync_queue_manager) {
