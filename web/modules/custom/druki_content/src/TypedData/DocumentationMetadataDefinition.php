468802e81a501cccad47510a0ab4c81119d9b91d
Niklan <niklanrus@gmail.com>
2022-01-18 21:22:54 +0500

    Simplified Documentation Metadata typed data definition. Reduced complexity.

--- web/modules/custom/druki_content/src/TypedData/DocumentationMetadataDefinition.php
+++ web/modules/custom/druki_content/src/TypedData/DocumentationMetadataDefinition.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\druki_content\TypedData;
 
 use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\DataDefinitionInterface;
 use Drupal\Core\TypedData\ListDataDefinition;
 use Drupal\Core\TypedData\MapDataDefinition;
 
@@ -19,13 +20,44 @@ final class DocumentationMetadataDefinition extends MapDataDefinition {
   public function getPropertyDefinitions(): array {
     $this->setPropertyDefinition('title', DataDefinition::create('string')->setRequired(TRUE));
     $this->setPropertyDefinition('slug', DataDefinition::create('string')->setRequired(TRUE));
-    $core_definition = DataDefinition::create('integer')
-      ->addConstraint('AllowedValues', ['8', '9', '10']);
-    $this->setPropertyDefinition('core', $core_definition);
-    $this->setPropertyDefinition('category', ContentCategoryDefinition::create());
+    $this->setPropertyDefinition('core', $this->getCoreDefinition());
+    $this->setPropertyDefinition('category', $this->getCategoryDefinition());
     $this->setPropertyDefinition('search-keywords', ListDataDefinition::create('string'));
     // @todo metatags.
     return parent::getPropertyDefinitions();
+  }
+
+  /**
+   * Gets definition for Drupal core.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface
+   *   A drupal core metadata definition.
+   */
+  protected function getCoreDefinition(): DataDefinitionInterface {
+    return DataDefinition::create('integer')
+      ->addConstraint('Range', [
+        'min' => 8,
+        'max' => 10,
+      ]);
+  }
+
+  /**
+   * Gets category metadata definition.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface
+   *   A data definition for category.
+   */
+  protected function getCategoryDefinition(): DataDefinitionInterface {
+    $order_definition = DataDefinition::create('integer')
+      ->addConstraint('Range', [
+        'min' => 0,
+        'max' => 1000,
+      ]);
+
+    return MapDataDefinition::create()
+      ->setPropertyDefinition('area', DataDefinition::create('string')->setRequired(TRUE))
+      ->setPropertyDefinition('order', $order_definition)
+      ->setPropertyDefinition('title', DataDefinition::create('string'));
   }
 
 }
